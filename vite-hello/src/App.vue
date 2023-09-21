51a04da25f505ce2f41d74fdbf7844ee38e0ba80
FedericaCampo97 <federica.campo97@gmail.com>
2023-09-21 16:56:38 +0200

    Exercise complete

--- vite-hello/src/App.vue
+++ vite-hello/src/App.vue
@@ -1,18 +1,22 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue';
-
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello Vite'
+    }
+  },
+  mounted() {
+    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    }
+  }
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1>
+      {{ message }}
+    </h1>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
